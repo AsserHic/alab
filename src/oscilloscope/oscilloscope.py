085c0754bd36629180c5b7c9b85bceaac9071cb6
Marko Laakso <marko.laakso@valuemotive.com>
2019-11-24 10:59:28 +0200

    Some basic IO functions for the scope

--- src/oscilloscope/oscilloscope.py
+++ src/oscilloscope/oscilloscope.py
@@ -6,6 +6,22 @@ import visa
 LOGGER = logging.getLogger(__file__)
 
 
+ERROR_STATUS = {
+    1: 'Unrecognized command/query header',
+    2: 'Invalid character',
+    3: 'Invalid separator',
+    4: 'Missing parameter',
+    5: 'Unrecognized keyword',
+    6: 'String error',
+    7: 'Parameter not allowed',
+    8: 'Command string too long',
+    9: 'Query not allowed',
+    10: 'Missing query mask',
+    11: 'Invalid parameter',
+    12: 'Parameter syntax error',
+    13: 'Too long filename',
+}
+
 class Oscilloscope:
 
     def __init__(self, addr):
@@ -16,12 +32,31 @@ class Oscilloscope:
                                                   read_termination='\n')
         self.connection.timeout = 4000
 
-    def write(self, command):
+    def write(self, command, block=True):
         self.connection.write(command)
+        self._check_for_errors()
+        if block:
+            self._wait_until_ready()
 
     def query(self, command):
         response = self.connection.query(command)
+        self._check_for_errors()
         return response
 
     def close(self):
         self.connection.close()
+
+    def _wait_until_ready(self):
+        self.connection.write('*OPC?')
+
+    def _check_for_errors(self):
+        status = self.connection.query('CMR?')
+        error = ERROR_STATUS.get(status)
+        if error:
+            raise IOError(error)
+
+    def reset(self):
+        self.write('*RST')
+
+    def auto_setup(self):
+        self.write('AUTO_SETUP')
--- src/oscilloscope/scope_cli.py
+++ src/oscilloscope/scope_cli.py
@@ -7,6 +7,8 @@ from oscilloscope import Oscilloscope
 def main():
     logging.basicConfig(level=logging.INFO)
     scope = Oscilloscope('TCPIP::192.168.2.3::5025::SOCKET')
+    #scope.reset()
+    #scope.auto_setup()
     print(scope.query('ALL_STATUS?'))
     scope.close()
 
