fe2d20c0963b2d865fd3dbdaa12f61488ae11e6d
Marko Laakso <marko.laakso@valuemotive.com>
2020-09-04 20:35:19 +0300

    Normalized signal

--- src/signal_art/svg_to_signal.py
+++ src/signal_art/svg_to_signal.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 
+import numpy
 import pandas
 from svg.path import parse_path, Path
 import xml.etree.ElementTree as ET
@@ -12,17 +13,29 @@ LOGGER = logging.getLogger(__file__)
 
 
 def path_to_df(path: Path):
+    LOGGER.info('Processing a path...')
     x = []
     y = []
     for step in path:
-        start = step.point(0)
-        x.append(start.real)
-        y.append(start.imag)
+        if step.length() == 0:
+            next
+        for t in numpy.arange(0.0, 1.0, 0.3):
+            loc = step.point(t)
+            x.append(loc.real)
+            y.append(loc.imag)
     df = pandas.DataFrame({
         'x': x,
         'y': y,
     })
+    df = df.apply(_normalize, axis=0)
+    df.y = 1 - df.y
     return df
+
+
+def _normalize(series):
+    low = series.min()
+    high = series.max()
+    return (series - low) / (high - low)
 
 
 def run(args: argparse.Namespace):
@@ -37,4 +50,5 @@ def run(args: argparse.Namespace):
                         filename)
             break
         df = path_to_df(parse_path(item.attrib['d']))
+
     show_xy_graph(df)
